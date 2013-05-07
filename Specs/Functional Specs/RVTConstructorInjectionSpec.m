c666593e90e1104f064de9154e37f0985191518e
Ryan Davies <ryan@ryandavies.net>
2013-05-07 21:41:08 +0100

    The providers approach is lengthy and tiresome. Removed relevant classes.
    Instead, 'dependencies' should be resolvable, as they were before, however a form of dependency must be resolvable using a provider. In this way, providers act as a convenient interface for building new objects, to be used in application or library code, rather than as the backbone for dependency injection on the whole.

--- Specs/Functional Specs/RVTConstructorInjectionSpec.m
+++ Specs/Functional Specs/RVTConstructorInjectionSpec.m
@@ -31,55 +31,55 @@
 
 @implementation RVTConstructorInjectionTests
 
-- (id<RVTProvider>)carWithInjector:(RVTInjector *)injector
-{
-    NSMutableArray *providers = [[NSMutableArray alloc] init];
-    providers[0] = [[RVTInjectionProvider alloc] initWithClass:[RVTDriver class] injector:injector];
-    providers[1] = [[RVTInjectionProvider alloc] initWithClass:[RVTEngine class] injector:injector];
-    providers[2] = [[RVTRecursiveProvider alloc] initWithProvider:[[RVTInjectionProvider alloc] initWithClass:[RVTWheelsProvider class] injector:injector] recursions:2];
-    return [[RVTInitializedObjectProvider alloc] initWithClass:[RVTCar class] selector:@selector(initWithDriver:engine:wheels:) providers:providers];
-}
-
-- (id<RVTProvider>)driverWithInjector:(RVTInjector *)injector
-{
-    NSMutableArray *providers = [[NSMutableArray alloc] init];
-    providers[0] = [[RVTValueProvider alloc] initWithValue:@"John Smith"];
-    return [[RVTInitializedObjectProvider alloc] initWithClass:[RVTDriver class] selector:@selector(initWithName:) providers:providers];
-}
-
-- (id<RVTProvider>)engineWithInjector:(RVTInjector *)injector
-{
-    return [[RVTInitializedObjectProvider alloc] initWithClass:[RVTEngine class] selector:@selector(init) providers:nil];
-}
-
-- (id<RVTProvider>)wheelsProviderWithInjector:(RVTInjector *)injector
-{
-    NSMutableArray *providers = [[NSMutableArray alloc] init];
-    providers[0] = [[RVTValueProvider alloc] initWithValue:[[RVTInjectionProvider alloc] initWithClass:[RVTWheel class] injector:injector]];
-    return [[RVTInitializedObjectProvider alloc] initWithClass:[RVTWheelsProvider class] selector:@selector(initWithWheelProvider:) providers:providers];
-}
-
-- (id<RVTProvider>)wheelWithInjector:(RVTInjector *)injector
-{
-    return [[RVTInitializedObjectProvider alloc] initWithClass:[RVTWheel class] selector:@selector(init) providers:nil];
-}
-
-- (void)test
-{
-    NSMutableDictionary *providers = [[NSMutableDictionary alloc] init];
-    RVTModule *module = [[RVTModule alloc] initWithProviders:providers];
-    RVTInjector *injector = [[RVTInjector alloc] initWithModule:module];
-    
-    module[[RVTCar    class]] = [self carWithInjector:injector];
-    module[[RVTDriver class]] = [self driverWithInjector:injector];
-    module[[RVTEngine class]] = [self engineWithInjector:injector];
-    module[[RVTWheel  class]] = [self wheelWithInjector:injector];
-    module[[RVTWheelsProvider class]] = [self wheelsProviderWithInjector:injector];
-    
-    NSLog(@"Injected car:    %@", [injector instanceOf:[RVTCar    class]]);
-    NSLog(@"Injected driver: %@", [injector instanceOf:[RVTDriver class]]);
-    NSLog(@"Injected engine: %@", [injector instanceOf:[RVTEngine class]]);
-    NSLog(@"Injected wheel:  %@", [injector instanceOf:[RVTWheel  class]]);
-}
+//- (id<RVTProvider>)carWithInjector:(RVTInjector *)injector
+//{
+//    NSMutableArray *providers = [[NSMutableArray alloc] init];
+//    providers[0] = [[RVTInjectionProvider alloc] initWithClass:[RVTDriver class] injector:injector];
+//    providers[1] = [[RVTInjectionProvider alloc] initWithClass:[RVTEngine class] injector:injector];
+//    providers[2] = [[RVTRecursiveProvider alloc] initWithProvider:[[RVTInjectionProvider alloc] initWithClass:[RVTWheelsProvider class] injector:injector] recursions:2];
+//    return [[RVTInitializedObjectProvider alloc] initWithClass:[RVTCar class] selector:@selector(initWithDriver:engine:wheels:) providers:providers];
+//}
+//
+//- (id<RVTProvider>)driverWithInjector:(RVTInjector *)injector
+//{
+//    NSMutableArray *providers = [[NSMutableArray alloc] init];
+//    providers[0] = [[RVTValueProvider alloc] initWithValue:@"John Smith"];
+//    return [[RVTInitializedObjectProvider alloc] initWithClass:[RVTDriver class] selector:@selector(initWithName:) providers:providers];
+//}
+//
+//- (id<RVTProvider>)engineWithInjector:(RVTInjector *)injector
+//{
+//    return [[RVTInitializedObjectProvider alloc] initWithClass:[RVTEngine class] selector:@selector(init) providers:nil];
+//}
+//
+//- (id<RVTProvider>)wheelsProviderWithInjector:(RVTInjector *)injector
+//{
+//    NSMutableArray *providers = [[NSMutableArray alloc] init];
+//    providers[0] = [[RVTValueProvider alloc] initWithValue:[[RVTInjectionProvider alloc] initWithClass:[RVTWheel class] injector:injector]];
+//    return [[RVTInitializedObjectProvider alloc] initWithClass:[RVTWheelsProvider class] selector:@selector(initWithWheelProvider:) providers:providers];
+//}
+//
+//- (id<RVTProvider>)wheelWithInjector:(RVTInjector *)injector
+//{
+//    return [[RVTInitializedObjectProvider alloc] initWithClass:[RVTWheel class] selector:@selector(init) providers:nil];
+//}
+//
+//- (void)test
+//{
+//    NSMutableDictionary *providers = [[NSMutableDictionary alloc] init];
+//    RVTModule *module = [[RVTModule alloc] initWithProviders:providers];
+//    RVTInjector *injector = [[RVTInjector alloc] initWithModule:module];
+//    
+//    module[[RVTCar    class]] = [self carWithInjector:injector];
+//    module[[RVTDriver class]] = [self driverWithInjector:injector];
+//    module[[RVTEngine class]] = [self engineWithInjector:injector];
+//    module[[RVTWheel  class]] = [self wheelWithInjector:injector];
+//    module[[RVTWheelsProvider class]] = [self wheelsProviderWithInjector:injector];
+//    
+//    NSLog(@"Injected car:    %@", [injector instanceOf:[RVTCar    class]]);
+//    NSLog(@"Injected driver: %@", [injector instanceOf:[RVTDriver class]]);
+//    NSLog(@"Injected engine: %@", [injector instanceOf:[RVTEngine class]]);
+//    NSLog(@"Injected wheel:  %@", [injector instanceOf:[RVTWheel  class]]);
+//}
 
 @end
