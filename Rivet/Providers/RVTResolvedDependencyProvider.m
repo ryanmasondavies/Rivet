7c62a6dd717781ca8639ea9fb453f05b76222c20
Ryan Davies <ryan@ryandavies.net>
2013-05-08 16:09:49 +0100

    Added resolved dependency provider.

--- Rivet/Providers/RVTResolvedDependencyProvider.m
+++ Rivet/Providers/RVTResolvedDependencyProvider.m
@@ -1,17 +1,17 @@
 // The MIT License
-// 
+//
 // Copyright (c) 2013 Ryan Davies
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,26 +20,26 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#ifndef _RIVET_
-#   define _RIVET_
+#import "RVTResolvedDependencyProvider.h"
+#import "RVTDependency.h"
 
-// dependencies
-#   import "RVTDependency.h"
-#   import "RVTDependencyMap.h"
-#   import "RVTInitializer.h"
-#   import "RVTProperty.h"
+@interface RVTResolvedDependencyProvider ()
+@property (strong, nonatomic) RVTDependency *dependency;
+@end
 
-// injection
-#   import "RVTInjector.h"
+@implementation RVTResolvedDependencyProvider
 
-// providers
-#   import "RVTObjectProvider.h"
-#   import "RVTResolvedDependencyProvider.h"
-#   import "RVTSequentialProvider.h"
-#   import "RVTValueProvider.h"
+- (id)initWithDependency:(RVTDependency *)dependency
+{
+    if (self = [self init]) {
+        self.dependency = dependency;
+    }
+    return self;
+}
 
-// scopes
-#   import "RVTScopeCache.h"
-#   import "RVTNoScope.h"
-#   import "RVTSingletonScope.h"
-#endif
+- (id)get
+{
+    return [[self dependency] resolve];
+}
+
+@end
