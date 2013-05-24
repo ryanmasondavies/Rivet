e3df8bb2bd3cfff093254f11795072e30887a986
Ryan Davies <ryan@ryandavies.net>
2013-05-24 17:27:41 +0100

    Renamed 'product' to 'dependency'.

---
+++ Rivet/RVTDependency.m
@@ -0,0 +1,61 @@
+// The MIT License
+//
+// Copyright (c) 2013 Ryan Davies
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "RVTDependency.h"
+
+@interface RVTDependency ()
+@property (strong, nonatomic, readwrite) Class klass;
+@property (strong, nonatomic, readwrite) NSString *name;
+@end
+
+@implementation RVTDependency
+
++ (id)dependencyWithClass:(Class)klass name:(NSString *)name
+{
+    return [[self alloc] initWithClass:klass name:name];
+}
+
+- (id)initWithClass:(Class)klass name:(NSString *)name
+{
+    if (self = [self init]) {
+        self.klass = klass;
+        self.name = name;
+    }
+    return self;
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    return [[[self class] alloc] initWithClass:[self klass] name:[self name]];
+}
+
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"<%@: %p, klass: '%@', name: '%@'>", [self class], self, [self klass], [self name]];
+}
+
+- (BOOL)isApplicableToDependency:(RVTDependency *)dependency
+{
+    return [self klass] == [dependency klass];
+}
+
+@end
